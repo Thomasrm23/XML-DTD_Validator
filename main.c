5b0efc4f5603242cd6d0ca4cc1275bc605d044d6
ephraim.amz <ephraimamezian@gmail.com>
2020-10-26 14:31:51 +0100

    Ajout fonction fileLength

--- main.c
+++ main.c
@@ -1,15 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <assert.h>
 
-int main()
+#include <sys/file.h>
+#include <strings.h>
+long fileLength(FILE* file);
+
+int main(int argc,char* argv[])
 {
     FILE* xmlFile;
-    xmlFile = fopen("dtdTest.xml","r");
+
+    xmlFile = fopen("fichierTest.xml","r");
     if(xmlFile == NULL){
         perror("File cannot be read because");
         exit(-1);
     }
 
+    long fLength = fileLength(xmlFile);
+    char fBuffer[fLength];
+
+    fread(fBuffer,fLength,1,xmlFile);
+
     fclose(xmlFile);
     return 0;
+}
+
+long fileLength(FILE* file){
+    assert(file != NULL);
+
+    fseek(file,0L,SEEK_END);
+    long length = ftell(file);
+    fseek(file,0L,SEEK_SET);
+    return length;
 }
