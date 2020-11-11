f9fa2eb9f9bff23be37d68943cfd894c022998ee
ephraim.amz <ephraimamezian@gmail.com>
2020-11-11 13:41:02 +0100

    Import zip Thomas

--- main.c
+++ main.c
@@ -1,39 +1,12 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <assert.h>
+#include "parsing_xml.h"
 
-
-/* Librairies potentiellement utiles
-#include <cytpe.h>
- */
-
-long filelength(FILE* file);
-
-
-int main(int argc,char* argv[]){
-    FILE* xmlFile;
-
-    xmlFile = fopen("fichierTest.xml","r");
-    if(xmlFile == NULL){
-        perror("File cannot be read because");
-        exit(1);
+int main(){
+    XMLDocument doc;
+    if (XMLDocument_load(&doc, "test_parse.xml")){
+        printf("%s: %s\n", doc.root->tag, doc.root->inner_text);
+        XMLDocument_free(&doc);
     }
-
-    long fLength = filelength(xmlFile);
-    char* fBuffer = (char *)malloc(fLength * sizeof(char*));
-    fread(fBuffer,fLength,1,xmlFile);
-
-    printf("%s\n",fBuffer);
-    fclose(xmlFile);
+//    else
+//        printf("no");
     return 0;
-}
-
-long filelength(FILE* file){
-    assert(file != NULL);
-
-    fseek(file,0L,SEEK_END);
-    long length = ftell(file);
-    fseek(file,0L,SEEK_SET);
-    return length;
 }
