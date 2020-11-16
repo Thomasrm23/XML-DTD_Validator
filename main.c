fad2f0053b3f759a6fe8a1a4108978532a64ef27
Thomasrm23 <57709671+Thomasrm23@users.noreply.github.com>
2020-11-16 18:33:23 +0100

    parse dtd

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include "parsing_xml.h"
+#include "parsing_dtd.h"
 
 int main(){
     XMLDocument doc;
@@ -6,7 +7,13 @@ int main(){
         printf("%s: %s\n", doc.root->tag, doc.root->inner_text);
         XMLDocument_free(&doc);
     }
-//    else
-//        printf("no");
+    
+    DTDDocument doc2;
+    if (DTDDocument_load(&doc2, "test_parse.dtd")){
+       // printf("%d\n", DTDDocument_load(&doc, "test_parse.xml"));
+
+        printf("%s\n %s\n %s\n %s\n %s\n"  , doc2.root->tag_type, doc2.root->tag_name, doc2.root->content, doc2.root->children[0], doc2.root->children[1]);
+
+    }
     return 0;
 }
