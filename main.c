67417b78da4ca3364f318f147dc7c00cdedfe1ae
Thomasrm23 <57709671+Thomasrm23@users.noreply.github.com>
2020-11-25 20:46:49 +0100

    partie 2 opérationnelle

--- main.c
+++ main.c
@@ -1,45 +1,24 @@
-#include <stdio.h>
 #include "parsing_xml.h"
 #include "parsing_dtd.h"
+#include "comparing.h"
 
-/**
-* Vérification de la présence d'un tag XML dans le DTD
-* @param[in] xmlTag : Le tag présent dans le fichier XML
-* @param[in] dtdTag : Le tag présent dans le fichier DTD
-* @param[out] boolean : Le tag XML est présent dans le DTD ou non
-*/
-bool isInDTD(char* xmlTag,char* dtdTag){
-    if (strcmp(xmlTag,dtdTag) == 0){
-        printf("Le tag %s est defini dans le DTD.",xmlTag);
-        return true;
-    }
-    else{
-        printf("Le tag %s n'est pas defini dans le DTD.",xmlTag);
-        return false;
-    }
-}
 
-int main() {
+int main(int argc, char**argv){
     XMLDocument doc;
-    char* xmlTag = NULL;
-    if (XMLDocument_load(&doc, "test_parse.xml")){
-      // printf("%s: %s\n", doc.root->tag, doc.root->inner_text);
-       xmlTag = doc.root->tag;
-    }
-
     DTDDocument doc2;
-    char* dtdTag = NULL;
-    if (DTDDocument_load(&doc2, "test_parse.dtd")){
-        // printf("%d\n", DTDDocument_load(&doc, "test_parse.xml"));
-        dtdTag = doc2.root->tag_name;
-        // printf("%s\n %s\n %s\n %s\n %s\n", doc2.root->tag_type,doc2.root->tag_name, doc2.root->children[0],doc2.root->children[1],doc2.root->children[2]);
+    if ((XMLDocument_load(&doc, "partie2_muliples.xml")) && (DTDDocument_load(&doc2, "partie2_muliples.dtd"))){
 
+
+
+
+        int result = isDTDGood(&doc, &doc2);
+
+        if (result == 1){
+            printf("\nDTD OK et XML correct\n");
+        }
+
+        XMLDocument_free(&doc);
     }
 
-    isInDTD(xmlTag,dtdTag);
-
-   // doc.root->;
-    XMLDocument_free(&doc);
-    // DTDDocument_free(&doc2);
     return 0;
 }
